ba28dea600117535fc15b167bd2b75e6e1eab6c0
A-K-0802 <aalok.thakkar01@gmail.com>
2024-04-04 16:53:51 +0530

    Trie created

--- Cap1.cpp
+++ Cap1.cpp
@@ -3,3 +3,55 @@
 #include<string>
 
 using namespace std;
+
+#define alphabets_in_english 26
+
+class trienode{
+    trienode* children[alphabets_in_english];
+
+    bool isend;
+    public:
+
+    friend void insert(trienode *root,string key);
+    friend bool search(trienode* root, string key);
+    friend trienode* getnode();
+};
+    trienode* getnode(){
+        trienode* node=new trienode;
+
+        node->isend=false;
+        for(int i=0;i<alphabets_in_english;i++){
+            node->children[i]=NULL;
+        }
+        return node;
+    }
+
+    void insert(trienode *root,string key){
+        trienode* temp=root;
+        for(int i=0;i<key.length();i++){
+            int index=key[i]-'a';
+            if(!temp->children[index]){
+                temp->children[index]=getnode();
+            }
+            temp=temp->children[index];
+        }
+        temp->isend=true;
+    }
+
+    bool search(trienode* root, string key){
+        trienode* temp= root;
+        for (int i = 0; i < key.length(); i++){
+            int index = key[i] - 'a';
+            if(!temp->children[index]){
+                return false;
+            }
+            temp=temp->children[index];
+        }
+        return temp->isend;
+    }
+
+
+int main(){
+    
+    return 0;
+}
