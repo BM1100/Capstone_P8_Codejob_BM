6e3219f4f03b14b5a2aa77a503323ee22eb950b1
A-K-0802 <aalok.thakkar01@gmail.com>
2024-04-06 17:41:53 +0530

    Using Doubly linked list to store correct words

--- Cap1.cpp
+++ Cap1.cpp
@@ -57,12 +57,30 @@ class DLL{
     void display(){
         node*temp=head;
         while(temp!=NULL){
-            cout<<temp->data<<" ";
+            cout<<temp->data<<endl;
             temp=temp->right;
         }
         cout<<endl;
     }
-}; DLL list_correct;
+
+    void pop(){
+        node* temp=head;
+        if(head->right=NULL){
+            head=NULL;
+        }
+        while(temp->right!=NULL){
+            temp=temp->right;
+        }
+        node*save = temp->left->right;
+        temp->left->right=NULL;
+        delete save;
+    }
+
+    friend void clear(DLL list);
+    
+};
+
+
 
 class trienode{
     trienode* children[alphabets_in_english];
@@ -73,10 +91,10 @@ class trienode{
     friend void insert(trienode *root,string key);
     friend bool search(trienode* root, string key);
     friend trienode* getnode();
-    friend string missingchar(trienode*root,string str);
-    friend void incorrect_arrange(string str, int l, int r,trienode*root);
-    friend void extrachar(string str,trienode* root);
-    friend void exchange_char(string str,trienode* root);
+    friend string missingchar(trienode*root,string str,DLL* list);
+    friend void incorrect_arrange(string str, int l, int r,trienode*root,DLL* list);
+    friend void extrachar(string str,trienode* root,DLL* list);
+    friend void exchange_char(string str,trienode* root,DLL* list);
 };
     trienode* getnode(){
         trienode* node=new trienode;
@@ -111,7 +129,7 @@ class trienode{
         }
         return temp->isend;
     }
-    string missingchar(trienode*root,string str){
+    string missingchar(trienode*root,string str,DLL list){
         int x = str.length();
         string s1;
         s1.append(str);
@@ -121,31 +139,32 @@ class trienode{
             string D=lower_alpha[i];
             word=s1;
             word.append(D);
-            incorrect_arrange(word,0,word.length()-1,root);
+            incorrect_arrange(word,0,word.length()-1,root,&list);
 
         }
 
         
     }
 
-    void incorrect_arrange(string str, int l, int r,trienode*root){ 
+    void incorrect_arrange(string str, int l, int r,trienode*root,DLL* list){ 
         string a=str;
         if (l == r){ 
             if(search(root,a)){
-                cout<< a <<endl;
-                list_correct.insert(a);
+                //cout<< a <<endl;
+                list->insert(a);
+                
             } 
         }
         else { 
             for (int i = l; i <= r; i++) { 
                 swap(a[l], a[i]); 
-                incorrect_arrange(a, l + 1, r,root); 
+                incorrect_arrange(a, l + 1, r,root,list); 
                 swap(a[l], a[i]); 
             } 
         } 
     } 
 
-    void extrachar(string str,trienode* root){
+    void extrachar(string str,trienode* root,DLL* list){
         string s2;
         for(int i=0;i<str.length();i++)
         {
@@ -156,8 +175,9 @@ class trienode{
             }
             s2.pop_back();
             if(search(root,s2)){
-                cout<< s2 <<endl;
-                list_correct.insert(s2);
+                //cout<< s2 <<endl;
+                list->insert(s2);
+                //list->display();
             } 
             
         }
@@ -183,15 +203,15 @@ class trienode{
         return stc;
     }
  
-    void exchange_char(string str, trienode* root){
+    void exchange_char(string str, trienode* root,DLL* list){
         string s2=str;
         string lower_alpha[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
         for(int i=0;i<s2.length();i++){
             for(int j=0;j<26;j++){
                 s2.replace(i,1,lower_alpha[j]);
                 if(search(root,s2)){
-                    cout<< s2 <<endl;
-                    list_correct.insert(s2);
+                    //cout<< s2 <<endl;
+                    list->insert(s2);
                 } 
             }
             s2=str;
@@ -217,20 +237,16 @@ int main(){
         }
         storage_file=store(sentence);
     }
-    // if(!search(root,"stac")){
-    //     missingchar(root,"stac");
-    //     incorrect_arrange("stac",0,sizeof("stac")-2,root);
-    //     extrachar("stac",root);
-    //     exchange_char("stac",root);
-    // }
     
     while(!storage_file.empty()){
+        DLL* list=new DLL();
         if(!search(root,storage_file.top())){
             cout<<"Incorrect spelling :"<<storage_file.top()<<endl;
-            incorrect_arrange(storage_file.top(),0,storage_file.top().length()-2,root);
-            extrachar(storage_file.top(),root);
-            exchange_char(storage_file.top(),root);
+            incorrect_arrange(storage_file.top(),0,storage_file.top().length()-2,root,list);
+            extrachar(storage_file.top(),root,list);
+            exchange_char(storage_file.top(),root,list);
         }
+        list->display();
         storage_file.pop();
     }
     
